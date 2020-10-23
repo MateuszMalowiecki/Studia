666b23d7d9e0b312c3d5536593c521ec7fd03b87
MateuszMalowiecki <33604498+MateuszMalowiecki@users.noreply.github.com>
2020-10-23 10:42:46 +0200

    Added L1Z1 from advanced cpp

---
+++ Zaawansowany c++ i STL/Lista 1/Zad1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "??-" << endl;
+    cout << "AAA" << endl;
+    return 0;
+}
+/*co zrobic - dodac opcje kompilacji -trigraphs*/
