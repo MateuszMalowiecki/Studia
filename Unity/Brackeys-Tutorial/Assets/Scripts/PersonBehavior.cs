76c1ad0996a9c7856190cac9113fb1c70aaf1ef5
MateuszMalowiecki <33604498+MateuszMalowiecki@users.noreply.github.com>
2020-10-24 10:21:16 +0200

    Added files from Brackeys tutorial

---
+++ Unity/Brackeys-Tutorial/Assets/Scripts/PersonBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PersonBehavior : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform playerpos;
+    public float forwardForce=200f;
+    public float sidewaysForce=200f;
+    
+    // Update is called once per frame
+    void Update() {
+        if (playerpos.position.y < 1.5) {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Unity/Brackeys-Tutorial/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PersonBehavior movement;
+    public GameManager gm;
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
