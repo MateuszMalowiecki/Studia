5bed2353b69810ae19800cc11f3ac10965851553
MateuszMalowiecki <33604498+MateuszMalowiecki@users.noreply.github.com>
2020-12-05 00:06:51 +0100

    Added Z2L9 from c++ course

---
+++ Zaawansowany c++ i STL/Lista 9/Zad2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <deque>
+#include <complex>
+#include <regex>
+
+using namespace std;
+
+bool match_to_date_regex(string& str) {
+    regex d("(31-(01|03|05|07|08|10|12)|30-(01|0[3-9]|1[0-2])|((0[1-9]|[1-2][0-9])-(0[1-9]|1[0-2])))-[0-9]{4}");
+    return regex_match(str, d);
+}
+
+
+int main() {
+    string s;
+   while(true) {
+        cout << "Give me expression and i will try if it is a valid date:" << endl;
+        getline(cin, s);
+        if(match_to_date_regex(s)) {
+            cout << "Expression is a valid date" << endl;
+        }
+        else {
+            cout << "Expression is not a valid date" << endl;
+        }
+    }
+    return 0;
+}
