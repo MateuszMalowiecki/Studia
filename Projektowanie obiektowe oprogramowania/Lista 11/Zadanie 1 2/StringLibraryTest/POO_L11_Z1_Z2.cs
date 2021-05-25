ae710dd857da80f2826076658179ac315755841e
MateuszMalowiecki <33604498+MateuszMalowiecki@users.noreply.github.com>
2021-05-25 20:09:49 +0200

    Added L11 from POO course

---
+++ Projektowanie obiektowe oprogramowania/Lista 11/Zadanie 1 2/StringLibraryTest/POO_L11_Z1_Z2.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace POO_L11_Z1_Z2 {
+    public class DependencyConstrutorAttribute: Attribute {}
+    public class DependencyPropertyAttribute : Attribute {}
+    public class SimpleContainer {
+        private Dictionary<Type, object> objects;
+        
+        private List<Type> singletonTypes;
+        
+        public SimpleContainer() {
+            objects=new Dictionary<Type, object>();
+            singletonTypes=new List<Type>();
+        }
+        public void RegisterType<T>( bool Singleton ) where T : class {
+            objects[typeof(T)] = typeof(T);
+            if(Singleton) {
+                singletonTypes.Add(typeof(T));
+            }
+            else {
+                singletonTypes.Remove(typeof(T));
+            }
+        }
+        
+        public void RegisterType<From, To>( bool Singleton ) where To : From {
+            objects[typeof(From)] = typeof(To);
+            if(Singleton) {
+                singletonTypes.Add(typeof(From));
+            }
+            else {
+                singletonTypes.Remove(typeof(From));
+            }
+        }
+        
+        public void RegisterInstance<T>( T Instance ) {
+            objects[typeof(T)]=Instance;
+        }
+
+        //Zadanie 1
+        public void ResolveProperty(object instance, List<Type> typesResolved) {
+            foreach (var property in instance.GetType().GetProperties()) {
+                if(Attribute.IsDefined(property, typeof(DependencyPropertyAttribute))
+                    && property.CanWrite
+                    && property.SetMethod.IsPublic) {
+                        property.SetValue(instance, Resolve(property.PropertyType, typesResolved));
+                }
+            }
+        }
+
+        public object Resolve(Type objectType, List<Type> typesResolved) {
+            if(typesResolved.Contains(objectType)) {
+                throw new ArgumentException("Detected cycle in derivation tree");
+            }
+            object hiddenObject;
+            if (!objects.TryGetValue(objectType, out hiddenObject)) {
+                //I can't use variable as generic type in RegisterType function, 
+                //when objectType is concrete, so I throw an exception instead.
+                throw new ArgumentException(String.Format("Not registered class {0} passed to function resolve", objectType));
+            }
+            if(hiddenObject is Type) {
+                typesResolved.Add(objectType);
+                ConstructorInfo[] constrs=((Type)hiddenObject).GetConstructors();
+                ConstructorInfo chosenConstr=Enumerable.ToList(constrs.OrderByDescending(constr => constr.GetParameters().Length))[0];
+                ConstructorInfo[] dependencyConstrs = Array.FindAll(constrs, constr => Attribute.IsDefined(constr, typeof(DependencyConstrutorAttribute)));
+                if(dependencyConstrs.Length == 1) {
+                    chosenConstr=dependencyConstrs[0];
+                }
+                object newObject = chosenConstr.Invoke(chosenConstr.GetParameters().Select(param => {
+                        return Resolve(param.ParameterType, typesResolved);
+                    }
+                ).ToArray());
+                ResolveProperty(newObject, typesResolved);
+                if (singletonTypes.Contains(objectType)) {
+                    objects[objectType]=newObject;
+                }
+                return newObject;
+            }
+            return hiddenObject;
+        }
+        
+        public T Resolve<T>() where T : class {
+            return (T)Resolve(typeof(T), new List<Type>());
+        }
+
+        //Zadanie 2
+        public void BuildUp<T>( T Instance ) {
+            ResolveProperty(Instance, new List<Type>());
+        }
+    }
+
+    //Zadanie 3
+    public delegate SimpleContainer ContainerProviderDelegate();
+
+    public class ServiceLocator {
+        private static ContainerProviderDelegate ContProv;
+        private static ServiceLocator Instance;
+        public static void SetContainerProvider( ContainerProviderDelegate ContainerProvider ) {
+            ContProv=ContainerProvider;
+        }
+        public static ServiceLocator Current {
+            get { 
+                if(Instance == null) {
+                    Instance=new ServiceLocator();
+                }
+                return Instance;
+            }
+        }
+        public T GetInstance<T>() where T : class {
+            if (typeof(T) == typeof(SimpleContainer)) {
+                return (T)(object)ContProv();
+            }
+            return ContProv().Resolve<T>();
+        }
+    }
+}
---
+++ Projektowanie obiektowe oprogramowania/Lista 11/Zadanie 1 2/StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,200 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POO_L11_Z1_Z2;
+using System;
+
+namespace StringLibraryTest {
+    [TestClass]
+    public class UnitTest1 {
+        interface IFoo {}
+        
+        class Foo : IFoo {}
+        
+        class Bar {
+            public Bar() {}
+            public Bar(Bar bar) {}
+        }
+        class Qux {
+            public Foo foo;
+            public Qux(Foo foo) {
+                this.foo=foo;
+            }
+        } 
+
+        class Baz {
+            [DependencyConstrutor]
+            public Baz() {}
+
+            public Baz(Baz baz) {}
+        }
+        public class A {
+            public A() { }
+            
+            [DependencyProperty]
+            public D TheD { get; set; } 
+        }
+        public class B {
+            public B() { }
+            
+            [DependencyProperty]
+            public D TheD { get; private set; } 
+        }
+        public class C {
+            public C() { }
+            
+            [DependencyProperty]
+            public D TheD { get; } 
+        }
+        public class D {} 
+        [TestMethod]
+        public void SingletonCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Foo>(true);
+            Foo f1 = c.Resolve<Foo>();
+            Foo f2 = c.Resolve<Foo>();
+            Assert.AreSame(f1, f2);
+        }
+        [TestMethod]
+        public void NormalCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Foo>(false);
+            Foo f1 = c.Resolve<Foo>();
+            Foo f2 = c.Resolve<Foo>();
+            Assert.AreNotSame(f1, f2);
+        }
+        [TestMethod]
+        public void InterfaceCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<IFoo, Foo>(false);
+            IFoo f = c.Resolve<IFoo>();
+            Assert.IsInstanceOfType(f, typeof(Foo));
+        }
+        [TestMethod]
+        public void InterfaceSingletonCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<IFoo, Foo>(true);
+            IFoo f1 = c.Resolve<IFoo>();
+            IFoo f2 = c.Resolve<IFoo>();
+            Assert.IsInstanceOfType(f1, typeof(Foo));
+            Assert.IsInstanceOfType(f2, typeof(Foo));
+            Assert.AreSame(f1, f2);
+        }
+        [TestMethod]
+        public void UnregisteredConcreteTypeCreation() {
+            SimpleContainer c = new SimpleContainer();
+            Assert.ThrowsException<ArgumentException>(() => {Foo f1 = c.Resolve<Foo>();});
+        }
+        [TestMethod]
+        public void UnregisteredAbstractTypeCreation() {
+            SimpleContainer c = new SimpleContainer();
+            Assert.ThrowsException<ArgumentException>(() => { IFoo f1 = c.Resolve<IFoo>(); });
+        }
+        [TestMethod]
+        public void InstanceCreation() {
+            SimpleContainer c = new SimpleContainer();
+            IFoo f1 = new Foo();
+            c.RegisterInstance<IFoo>( f1 );
+            IFoo f2 = c.Resolve<IFoo>();
+            Assert.IsInstanceOfType(f2, typeof(Foo));
+            Assert.AreSame(f1, f2);
+        }
+        [TestMethod]
+        public void CyclicCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Bar>(false);
+            Assert.ThrowsException<ArgumentException>(() => { Bar b = c.Resolve<Bar>(); });
+        }
+        [TestMethod]
+        public void RegisteredParametersCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Foo>(false);
+            c.RegisterType<Qux>(false);
+            Qux q = c.Resolve<Qux>();
+            Assert.IsInstanceOfType(q, typeof(Qux));
+            Assert.IsInstanceOfType(q.foo, typeof(Foo));
+        }
+        [TestMethod]
+        public void UnregisteredParametersCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Qux>(false);
+            Assert.ThrowsException<ArgumentException>(() => { Qux q = c.Resolve<Qux>(); });
+        }
+        [TestMethod]
+        public void RegisteredParametersSingletonCreation() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Foo>(false);
+            c.RegisterType<Qux>(true);
+            Qux q = c.Resolve<Qux>();
+            Qux q2 = c.Resolve<Qux>();
+            Assert.AreSame(q, q2);
+        }
+        [TestMethod]
+        public void DependencyConstrutorTest() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<Baz>(false);
+            Baz b = c.Resolve<Baz>();
+            Assert.IsInstanceOfType(b, typeof(Baz));
+        }
+        [TestMethod]
+        public void PropertyTest() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<A>(false);
+            c.RegisterType<D>(false);
+            Assert.IsInstanceOfType(c.Resolve<A>().TheD, typeof(D));
+        }
+        [TestMethod]
+        public void PropertyPrivateSetterTest() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<B>(false);
+            c.RegisterType<D>(false);
+            Assert.IsNull(c.Resolve<B>().TheD);
+        }
+        [TestMethod]
+        public void PropertyNoSetterTest() {
+            SimpleContainer c = new SimpleContainer();
+            c.RegisterType<C>(false);
+            c.RegisterType<D>(false);
+            Assert.IsNull(c.Resolve<C>().TheD);
+        }
+
+        [TestMethod]
+        public void BuildUpTest() {
+            SimpleContainer c = new SimpleContainer();
+            A TheA = new A();
+            c.RegisterType<D>(false);
+            Assert.IsNull(TheA.TheD);
+            c.BuildUp( TheA );
+            Assert.IsNotNull(TheA.TheD); 
+        }
+
+        [TestMethod]
+        public void ServiceLocatorSingletonTest() {
+            SimpleContainer c = new SimpleContainer();
+            ContainerProviderDelegate containerProvider = () => c;
+            ServiceLocator.SetContainerProvider( containerProvider );
+            ServiceLocator s=ServiceLocator.Current;
+            SimpleContainer c1=s.GetInstance<SimpleContainer>();
+            SimpleContainer c2=s.GetInstance<SimpleContainer>();
+            Assert.AreSame(c1, c2);
+        }
+        
+        [TestMethod]
+        public void ServiceLocatorTest() {
+            ContainerProviderDelegate containerProvider = () => new SimpleContainer();
+            ServiceLocator.SetContainerProvider( containerProvider );
+            ServiceLocator s=ServiceLocator.Current;
+            SimpleContainer c1=s.GetInstance<SimpleContainer>();
+            SimpleContainer c2=s.GetInstance<SimpleContainer>();
+            Assert.AreNotSame(c1, c2);
+        }
+
+        [TestMethod]
+        public void ServiceLocatorGetInstanceTest() {
+            SimpleContainer c = new SimpleContainer();
+            ContainerProviderDelegate containerProvider = () => c;
+            ServiceLocator.SetContainerProvider( containerProvider );
+            c.RegisterType<Baz>(false);
+            Baz baz = ServiceLocator.Current.GetInstance<Baz>();
+            Assert.IsNotNull(baz);
+        }
+    }
+}
